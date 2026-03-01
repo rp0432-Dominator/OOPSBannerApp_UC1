a0a0264ac37800883155a7e717b1f00d716e1384
Rishiraj Phukan <rishirajphukan@Rishirajs-MacBook-Pro-4.local>
2026-03-02 04:23:52 +0530

    UC2: Print OOPS banner using ASCII 7-line grid

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,22 @@
 /**
  * OOPSBannerApp
  *
- * UC1: Print literal text "OOPS"
+ * UC2: Print "OOPS" as ASCII Banner
  *
  * @author Rishiraj
- * @version 1.0
+ * @version 2.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" *****    *****    *****    ***** ");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  ******    ***** ");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println("*     *  *     *  *        *     *");
+        System.out.println(" *****    *****   *         ***** ");
+
     }
 }
